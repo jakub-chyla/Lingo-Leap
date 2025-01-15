e4e712a5016443de99211c3b7341037c31b12447
jakub-chyla <jakub.bchyla@gmail.com>
2025-01-15 17:58:30 +0100

    copied stripe

---
+++ Lingoleap - BE/src/main/java/com/lingo_leap/dto/ProductCheckoutController.java
@@ -0,0 +1,26 @@
+package com.lingo_leap.dto;
+
+import com.lingo_leap.service.StripeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/product/v1")
+public class ProductCheckoutController {
+
+    private final StripeService stripeService;
+
+    @PostMapping("/checkout")
+    public ResponseEntity<StripeResponse> checkoutProducts(@RequestBody ProductRequest productRequest) {
+        StripeResponse stripeResponse = stripeService.checkoutProducts(productRequest);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(stripeResponse);
+    }
+}
