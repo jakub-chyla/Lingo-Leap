97ae7d7b2760372a3be0058549661f50077a5fa2
jakub-chyla <jakub.bchyla@gmail.com>
2025-02-25 17:16:29 +0100

    added about

--- Lingoleap - FE/src/app/page/about/about.component.ts
+++ Lingoleap - FE/src/app/page/about/about.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatCard, MatCardContent, MatCardFooter} from "@angular/material/card";
+import {MatIcon} from "@angular/material/icon";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardContent,
+    MatCardFooter,
+    MatIcon,
+    MatIconButton,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  navigateToMain(): void {
+    this.router.navigate(['/main']);
+  }
 }
