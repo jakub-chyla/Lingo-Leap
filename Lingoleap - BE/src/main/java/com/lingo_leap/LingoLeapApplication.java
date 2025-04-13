5832687755f23b49b00e388d47851fb0c033d002
jakub-chyla <jakub.bchyla@gmail.com>
2025-04-13 17:57:56 +0200

    added footer

--- Lingoleap - BE/src/main/java/com/lingo_leap/LingoLeapApplication.java
+++ Lingoleap - BE/src/main/java/com/lingo_leap/LingoLeapApplication.java
@@ -3,11 +3,15 @@ package com.lingo_leap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
 public class LingoLeapApplication {
 
 	public static void main(String[] args) {
+		System.out.println(LocalDate.now());
 		SpringApplication.run(LingoLeapApplication.class, args);
+
 	}
 
 }
