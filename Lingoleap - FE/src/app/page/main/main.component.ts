53a3f77306a5aca43a0c8b37704c5ca8c694a6b2
jakub-chyla <jakub.bchyla@gmail.com>
2025-06-25 15:51:35 +0200

    fixed promise blob error

--- Lingoleap - FE/src/app/page/main/main.component.ts
+++ Lingoleap - FE/src/app/page/main/main.component.ts
@@ -167,10 +167,20 @@ export class MainComponent implements OnInit {
   }
 
   private playSound(data: Uint8Array) {
-    const blob = new Blob([data], {type: 'audio/mp3'});
-    let objectUrl = URL.createObjectURL(blob);
+    const blob = new Blob([data], { type: 'audio/mp3' });
+    const objectUrl = URL.createObjectURL(blob);
+
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
     this.audio.src = objectUrl;
-    this.audio.play();
+    this.audio.load();
+
+    this.audio.oncanplaythrough = () => {
+      this.audio.play().catch(err => {
+        console.warn('Audio playback error:', err);
+      });
+    };
   }
 
   private settingInit() {
